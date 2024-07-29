62c2288ec33627da3829d3a0006196df484d302e
Joctan <perfect.altruistic@gmail.com>
2024-07-29 12:42:54 -0400

    Lab 02: area of a circle

---
+++ practicals/labs/lab_02/AreaOfCircle.java
@@ -0,0 +1,19 @@
+package practicals.labs.lab_02;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the radius of the circle: ");
+            double radius = input.nextDouble();
+
+            double area = radius * radius * Math.PI;
+
+            System.out.println("Area is: " + area);
+        }catch(Exception e){
+            System.err.println("an error occured please try again" + e);
+        }
+    }
+}
