f966eccf5c39b9670c8d47d0787c078e3fa6d65e
Joctan <perfect.altruistic@gmail.com>
2024-08-01 17:38:59 -0400

    Chapter_01: part 1: Java Print Statements

---
+++ chapter_01/part_01/Output.java
@@ -0,0 +1,24 @@
+package chapter_01.part_01;
+
+// OUTPUT  IN JAVA
+
+public class Output {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Am Learning Java");
+        System.out.println("It is so cool and Awesome");
+
+/*
+    In Main.java File you learned that You can use a println() Method to
+    output / print text in Java, you can also add more println() Methods as you can.
+    Note that println() Will add a new line for each method.
+
+*/
+        System.out.println("On a new line.");
+        System.out.print("This will not start at a new line.");
+/*
+   When Working with text as above remember to insert your message in double quotes,
+   if you forget the double quotes an error occurs.
+ */
+    }
+}
