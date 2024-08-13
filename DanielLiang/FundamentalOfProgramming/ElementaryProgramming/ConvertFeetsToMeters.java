3d257fa61d0b8932a1227a71b8e738543cab5a54
Joctan <perfect.altruistic@gmail.com>
2024-08-13 01:39:05 -0400

    Fundamentals: 2.1: Convert feet into meters

---
+++ DanielLiang/FundamentalOfProgramming/ElementaryProgramming/ConvertFeetsToMeters.java
@@ -0,0 +1,23 @@
+package DanielLiang.FundamentalOfProgramming.ElementaryProgramming;
+
+import java.util.Scanner;
+
+public class ConvertFeetsToMeters {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            // user prompt
+            System.out.println("\n\tFeet to Meter Converter.");
+            System.out.print("\tEnter a Value for feet: ");
+            double feets = input.nextDouble();
+
+            // Scanner object closed
+            input.close();
+            // calculation
+            double meters = feets * 0.305;
+            System.out.println(feets +  " feet is " + String.format("%.4f", meters) + " in meters");
+        }catch (Exception e) {
+            System.out.println("an error occurred. " + e);
+        }
+    }
+}
