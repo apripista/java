a537469917dc28d776615945aef8700bca386680
Joctan <perfect.altruistic@gmail.com>
2024-08-13 01:33:35 -0400

    Fundamentals: 2.1: Convert Celsius to Fahrenheit

---
+++ DanielLiang/FundamentalOfProgramming/ElementaryProgramming/Acceleration.java
@@ -0,0 +1,28 @@
+package DanielLiang.FundamentalOfProgramming.ElementaryProgramming;
+
+import java.util.Scanner;
+
+public class Acceleration {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            // user prompt:
+            // starting velocity, final velocity and time taken
+            System.out.println("\n\tEnter your data in m and seconds. \n");
+            System.out.print("\tStarting velociy: ");
+            double initial_velocity = input.nextDouble();
+
+            System.out.print("\tFinal Velocity: ");
+            double final_velocity = input.nextDouble();
+
+            System.out.print("\tTime taken: ");
+            double time = input.nextDouble();
+
+            // acceleration calculations
+            double acceleration = (final_velocity - initial_velocity) / time;
+            System.out.println("\nAcceletation is: " + String.format("%.4f",acceleration) + "m/s^2");
+        }catch(Exception e){
+            System.err.println("\nan error occurred: " + e);
+        }
+    }
+}
