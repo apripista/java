d370a9b23b41f4cea74def9508af01485118b1ae
Joctan <perfect.altruistic@gmail.com>
2024-08-15 01:38:39 -0400

    Lab 04: even number finder up to 100

---
+++ practicals/labs/lab_04/EvenNumbers.java
@@ -0,0 +1,11 @@
+package labs.lab_04;
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        // loop for even numbers
+        for(int even = 0; even <= 100; even+=2){
+            System.out.println(even + " is even number");
+        }
+    }
+}
