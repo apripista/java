6d900fa23c2a1775419c12a3d2570205c67b2ddd
Joctan <perfect.altruistic@gmail.com>
2024-08-15 01:35:23 -0400

    Lab 02: sum of two integers

---
+++ practicals/labs/lab_02/SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+package practicals.labs.lab_02;
+
+public class SumOfTwoIntegers {
+
+    public static void main(String[] args) {
+
+        double numberone = 56.7, numbertwo = 43.948; // variable declaration
+        double sum = numberone + numbertwo; // calculations
+
+        // output
+        System.out.println("sum of " + numberone + " and " + numbertwo + " is: " + sum);
+    }
+}
