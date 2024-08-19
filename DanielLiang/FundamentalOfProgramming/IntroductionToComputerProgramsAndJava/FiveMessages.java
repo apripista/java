62309e8f7d9833c752f8992492cb5eee3edf0d05
Joctan <perfect.altruistic@gmail.com>
2024-08-19 02:05:21 -0400

    Fundamentals: 1.2: Display five messages

---
+++ DanielLiang/FundamentalOfProgramming/IntroductionToComputerProgramsAndJava/FiveMessages.java
@@ -0,0 +1,14 @@
+package DanielLiang.FundamentalOfProgramming.IntroductionToComputerProgramsAndJava;
+
+public class FiveMessages {
+
+    public static void main(String[] args) {
+        // five messages
+        System.out.println("\n");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
