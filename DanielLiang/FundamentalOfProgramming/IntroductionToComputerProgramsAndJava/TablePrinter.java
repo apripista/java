1e0a756fbc38028b230a86eaf8f9fd9327bc26e8
Joctan <perfect.altruistic@gmail.com>
2024-08-19 02:12:44 -0400

    Fundamentals: 1.4: Print a table

---
+++ DanielLiang/FundamentalOfProgramming/IntroductionToComputerProgramsAndJava/TablePrinter.java
@@ -0,0 +1,22 @@
+package DanielLiang.FundamentalOfProgramming.IntroductionToComputerProgramsAndJava;
+
+public class TablePrinter {
+    public static void main(String[] args) {
+
+        System.out.println("\n");;
+        // Header row
+        System.out.println("a\ta2\ta3");
+        
+        // Values for a
+        System.out.println("1\t1\t1");
+                
+        // Values for a^2
+        System.out.println("2\t4\t8");
+        
+        // Values for a^3
+        System.out.println("3\t9\t27");
+
+        System.out.println("4\t8\t64");
+
+    }
+}
