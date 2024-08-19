0a0b765ceff4190f114fa6620fc6b6128a5f9a4e
Joctan <perfect.altruistic@gmail.com>
2024-08-19 02:15:51 -0400

    Fundamentals: 1.3: Display a pattern

---
+++ DanielLiang/FundamentalOfProgramming/IntroductionToComputerProgramsAndJava/PrintJavaPattern.java
@@ -0,0 +1,15 @@
+package DanielLiang.FundamentalOfProgramming.IntroductionToComputerProgramsAndJava;
+
+public class PrintJavaPattern {
+    public static void main(String[] args) {
+        // JAVA pattern
+        System.out.println("    J     A   V     V   A");
+        System.out.println("    J    A A   V   V   A A");
+        System.out.println("    J   AAAAA   V V   AAAAA");
+        System.out.println("J J J  A     A   V   A     A");
+        
+        // Empty line
+        System.out.println();
+        
+    }
+}
