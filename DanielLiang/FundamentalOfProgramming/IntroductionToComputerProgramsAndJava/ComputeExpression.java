b21f06f79c422625ce5ad764e13421c9c5741e6d
Joctan <perfect.altruistic@gmail.com>
2024-08-19 02:14:52 -0400

    Fundamentals: 1.5: Compute expressions

---
+++ DanielLiang/FundamentalOfProgramming/IntroductionToComputerProgramsAndJava/ComputeExpression.java
@@ -0,0 +1,12 @@
+package DanielLiang.FundamentalOfProgramming.IntroductionToComputerProgramsAndJava;
+
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+        // expression
+        double resutl = ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5);
+        
+        // output result.
+        System.out.println("\nResult is: " + resutl);
+    }
+}
