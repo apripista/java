47eae985c2516ec0f83d745e7cbfccc8aac7a4d6
Joctan <perfect.altruistic@gmail.com>
2024-08-19 02:06:41 -0400

    Fundamentals: 1.1: ThreeMessages.java

---
+++ DanielLiang/FundamentalOfProgramming/IntroductionToComputerProgramsAndJava/ThreeMessages.java
@@ -0,0 +1,11 @@
+package DanielLiang.FundamentalOfProgramming.IntroductionToComputerProgramsAndJava;
+
+public class ThreeMessages{
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is fun");
+    
+    }
+}
