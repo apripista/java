600dacd3c86f64eb61e7539bf68e2f263dab9071
Joctan <perfect.altruistic@gmail.com>
2024-08-27 02:44:53 -0400

    Fundamentals: 2.18: Print a table

---
+++ DanielLiang/FundamentalOfProgramming/ElementaryProgramming/TablePrinter.java
@@ -0,0 +1,15 @@
+package DanielLiang.FundamentalOfProgramming.ElementaryProgramming;
+
+public class TablePrinter {
+
+    public static void main(String[] args) {
+        System.out.println("\ta \tb \tpow(a, b)");
+        System.out.println("");
+        System.out.println("\t1\t2\t1");
+        System.out.println("\t2\t3\t8");
+        System.out.println("\t3\t4\t81");
+        System.out.println("\t4\t5\t1024");
+        System.out.println("\t5\t6\t15625");
+        
+    }
+}
