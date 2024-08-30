0dc6cf171b4d2c5fe228f1d66763b426fe2b6c22
Joctan <perfect.altruistic@gmail.com>
2024-08-30 10:38:07 -0400

    Lab 04: Summation of Counting Integers

---
+++ practicals/labs/lab_04/SumOfCountingIntegers.java
@@ -0,0 +1,14 @@
+package labs.lab_04;
+
+public class SumOfCountingIntegers {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 0; i <=200; i++){
+            sum += i;
+        }
+        System.out.println("sum of first 200 counting number is:");
+        System.out.println("sum:  " + sum);
+
+    }
+}
