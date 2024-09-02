887cb67e9c349c9c583c10ca5f88c1ed84f73178
Joctan <perfect.altruistic@gmail.com>
2024-09-02 02:42:21 -0400

    Lab 01: character initialization

---
+++ practicals/labs/lab_01/CharInitialization.java
@@ -0,0 +1,11 @@
+package labs.lab_01;
+
+public class CharInitialization {
+    public static void main(String[] args) {
+        // Predefined value
+        char charValue = 'A';
+        
+        // Display the char value
+        System.out.println("Char value: " + charValue);
+    }
+}
