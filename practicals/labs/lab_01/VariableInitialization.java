e3f6ede071c9dbf84a8b68882fb268095343b03c
Joctan <perfect.altruistic@gmail.com>
2024-09-03 04:08:39 -0400

    Lab 01: variable initialization

---
+++ practicals/labs/lab_01/VariableInitialization.java
@@ -0,0 +1,15 @@
+package labs.lab_01;
+
+public class VariableInitialization {
+    public static void main(String[] args) {
+        // Predefined values
+        int num1 = 10;
+        int num2 = 20;
+        float floatValue = 3.14f;
+        
+        // Display the values
+        System.out.println("Value of num1: " + num1);
+        System.out.println("Value of num2: " + num2);
+        System.out.println("Value of floatValue: " + floatValue);
+    }
+}
