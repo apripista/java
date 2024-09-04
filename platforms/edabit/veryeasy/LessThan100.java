b18a4b416b4751c08722ddfdca365fbac4d6cd9b
Joctan <perfect.altruistic@gmail.com>
2024-09-04 10:06:12 -0400

    edabit: very easy: less than 100

--- platforms/edabit/veryeasy/LessThan100.java
+++ platforms/edabit/veryeasy/LessThan100.java
@@ -1,6 +1,8 @@
 public class LessThan100 {
   public static boolean lessThan100(int a, int b) {
 		int result = a + b;
+		// conditions to check if 
+		// the integers are less than 100 or not
 		if(result < 100){
 			return true;
 		}
@@ -8,6 +10,7 @@ public class LessThan100 {
   }
 	
 	public static void main(String[] args){
+		// calling the function and printing the output
 		System.out.println(lessThan100(78, 98));
 	}
 }
