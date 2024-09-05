da1e092382f836918fa3716a42a4e7cd986044d5
Joctan <perfect.altruistic@gmail.com>
2024-09-05 01:52:28 -0400

    Lab 01: calculate area of the rectangle

--- practicals/labs/lab_01/RectangleAreaCalculator.java
+++ practicals/labs/lab_01/RectangleAreaCalculator.java
@@ -5,11 +5,11 @@ public class RectangleAreaCalculator {
         // Predefined values
         double length = 4.0; // inches
         double width = 8.0;  // inches
-        
+
         // Calculate the area of the rectangle
         double area = length * width;
-        
-        // Display the result
+
+        // Output the result
         System.out.println("Length of the rectangle: " + length + " inches");
         System.out.println("Width of the rectangle: " + width + " inches");
         System.out.println("Area of the rectangle: " + area + " square inches");
