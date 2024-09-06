62d317521baa3b558f3f23584b87c85ac0546838
Joctan <perfect.altruistic@gmail.com>
2024-09-06 08:41:11 -0400

    edabit: very easy: less than or equal to zero

---
+++ platforms/edabit/veryeasy/LessThanOrEqualsToZero.java
@@ -0,0 +1,12 @@
+public class LessThanOrEqualsToZero {
+    public static boolean lessThanOrEqualToZero(int num) {
+      if(num <= 0){
+				return true;
+			}
+			else
+				return false;
+    }
+	public static void main(String[] args){
+		lessThanOrEqualToZero(9);
+	}
+}
