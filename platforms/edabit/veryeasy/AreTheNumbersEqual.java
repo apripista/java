6331e12fad5a70b4322ecb91aa458e324562b6c1
Joctan <perfect.altruistic@gmail.com>
2024-09-06 08:40:30 -0400

    edabit: very easy: are the numbers equal

---
+++ platforms/edabit/veryeasy/AreTheNumbersEqual.java
@@ -0,0 +1,11 @@
+public class AreTheNumbersEqual{
+	public static boolean isEqual(int num1, int num2) {
+		if(num1 == num2){
+			return true;
+		}
+		else return false;
+  }
+	public static void main(String[] args){
+		System.out.println(isEqual(7, 9));
+	}
+}
