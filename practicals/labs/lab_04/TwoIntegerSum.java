9f802260f05857ac09808d81065f854e67e53cc1
Joctan <octmalt@gmail.com>
2024-11-27 23:38:22 +0300

    Lab 04: Sum of Two Integers

---
+++ practicals/labs/lab_04/TwoIntegerSum.java
@@ -0,0 +1,13 @@
+package labs.lab_04;
+
+public class TwoIntegerSum{
+
+    public static void main(String[] args) {
+        double number = 56;
+        double integer = 45;
+        double sum = number + integer;
+
+        System.out.println("Numbers are: " + number + " and " +integer);
+        System.out.println("The sum is: " + sum);
+    }
+}
